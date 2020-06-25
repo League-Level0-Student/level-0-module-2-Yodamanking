c18c9c75670e13190ddc9e49d6f20afa504fd760
root <root@7f03b5002ca3>
2020-06-25 22:00:05 +0000

    Validation

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -11,13 +11,17 @@ public class Validation {
 	public static void main(String[] args) {
 		
 		Random randomMaker = new Random();
+		for (int i = 0; i < 5; i++) {
+			int randomNumber = randomMaker.nextInt(5);
+			if(randomNumber == 0)
+		}
 		
-		int randomNumber = randomMaker.nextInt(5);
+		
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
